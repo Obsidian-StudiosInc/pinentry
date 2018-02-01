7de186e3db73f74347b13ccf5d730a7c677f6681
William L. Thomson Jr <wlt@o-sinc.com>
2018-01-31 22:49:47 -0500

    ci: Fix travis wrapper, add missing echos, could move to install?

--- .travis-prep.sh
+++ .travis-prep.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-"@set UPDATED "`date +"%d %B %Y"`"" > doc/version.texi
-"@set UPDATED-MONTH "`date +"%d %B"`"" >> doc/version.texi
-"@set EDITION ${MY_PV}" >> doc/version.texi
-"@set VERSION ${MY_PV}" >> doc/version.texi
+echo "@set UPDATED "`date +"%d %B %Y"`"" > doc/version.texi
+echo "@set UPDATED-MONTH "`date +"%d %B"`"" >> doc/version.texi
+echo "@set EDITION ${MY_PV}" >> doc/version.texi
+echo "@set VERSION ${MY_PV}" >> doc/version.texi
 
 autoreconf -f -i
 automake
